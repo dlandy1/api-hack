06bc3be3bb930c5d1be210fd857881dc6ea9b2ea
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-07-25 02:04:53 -0400

    k

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var Apple = function(tags) {
  })
 	 $("#one").on("click", function(){
 	 	$(".template").remove();
+	 	$(".search").hide();
 	 $.getJSON(
 	 "http://www.reddit.com/r/apple/hot.json?jsonp=?",
         function foo(data)
@@ -31,12 +32,13 @@ var Apple = function(tags) {
  })
 	 $("#two").on("click", function(){
 	 	 $(".template").remove();
+	 	 $(".search").hide();
 	 $.getJSON(
 	 "http://www.reddit.com/r/google/hot.json?jsonp=?",
         function foo(data)
         {
           $.each(
-            data.data.children.slice(0, 10),
+            data.data.children.slice(0, 25),
             function (i, post) {
               $(".feed").append( '<div class = "template"> <p class="title"> <br> <a href ="' + post.data.url + '">'  + post.data.title + '</p> </a> <p class="text">  <br> Upvotes:' + post.data.ups + '</p> </div' );
  
@@ -46,12 +48,13 @@ var Apple = function(tags) {
  })
 	  $("#three").on("click", function(){
 	 	 $(".template").remove();
+	 	 $(".search").hide();
 	 $.getJSON(
 	 "http://www.reddit.com/r/tesla/hot.json?jsonp=?",
         function foo(data)
         {
           $.each(
-            data.data.children.slice(0, 10),
+            data.data.children.slice(0, 25),
             function (i, post) {
               $(".feed").append( '<div class = "template"> <p class="title"> <br> <a href ="' + post.data.url + '">'  + post.data.title + '</p> </a> <p class="text">  <br> Upvotes:' + post.data.ups + '</p> </div' );
  
@@ -61,12 +64,13 @@ var Apple = function(tags) {
  })
 	   $("#four").on("click", function(){
 	 	 $(".template").remove();
+	 	 $(".search").hide();
 	 $.getJSON(
 	 "http://www.reddit.com/r/facebook/hot.json?jsonp=?",
         function foo(data)
         {
           $.each(
-            data.data.children.slice(0, 10),
+            data.data.children.slice(0, 25),
             function (i, post) {
               $(".feed").append( '<div class = "template"> <p class="title"> <br> <a href ="' + post.data.url + '">'  + post.data.title + '</p> </a> <p class="text">  <br> Upvotes:' + post.data.ups + '</p> </div' );
  
@@ -75,28 +79,52 @@ var Apple = function(tags) {
         })
  })
 	    $("#five").on("click", function(){
-	 	 $(".template").remove();
-	 $.getJSON(
-	 "http://www.reddit.com/r/amazon/hot.json?jsonp=?",
+	     var link = [];
+	     $(".template").remove();
+	     $(".feed").hide();
+	 	 $(".search").show();
+	 	 $(".search").keydown(function(event) {
+	  		if (event.which == 13){
+	  			var value = $("input").val().trim();
+	  			if (value.length == 0){
+	  				alert("put in text")
+	  				}
+	  			else {
+	  			 $(".feed").show();
+	  			 $(".template").remove();
+	 	  $.getJSON(
+	 'http://www.reddit.com/r/' + value + '/hot.json?jsonp=?',
         function foo(data)
         {
           $.each(
-            data.data.children.slice(0, 10),
+            data.data.children.slice(0, 25),
             function (i, post) {
+            	link.push(1);
+            	console.log(link.length);
+ 			if(link.length == 0){
+ 				alert("no existing subreddit")
+ 			}
               $(".feed").append( '<div class = "template"> <p class="title"> <br> <a href ="' + post.data.url + '">'  + post.data.title + '</p> </a> <p class="text">  <br> Upvotes:' + post.data.ups + '</p> </div' );
- 
-            }
+ 			}
           )
         })
- })
+	 	  if(link.length == 0){
+ 				alert("no existing subreddit")
+ 			}
+}
+}
+})
+})
 	     $("#zero").on("click", function(){
+	     $(".search").hide();
 	 	 $(".template").remove();
+	 	 $(".search").show();
 	 $.getJSON(
 	 "http://www.reddit.com/r/technology/hot.json?jsonp=?",
         function foo(data)
         {
           $.each(
-            data.data.children.slice(0, 10),
+            data.data.children.slice(0, 25),
             function (i, post) {
               $(".feed").append( '<div class = "template"> <p class="title"> <br> <a href ="' + post.data.url + '">'  + post.data.title + '</p> </a> <p class="text">  <br> Upvotes:' + post.data.ups + '</p> </div' );
  
