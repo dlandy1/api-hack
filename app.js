22318f761bc2f49af971fc6512291b8ae0c87085
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-08-01 16:12:58 -0400

    ala

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready( function() {
 	Apple()
 });
 var Apple = function() {
+  $("#zero").css('background-color', 'rgba(0, 153, 255, 0.21)');
 	 $.getJSON(
 	 "http://www.reddit.com/r/technology/hot.json?jsonp=?",
         function foo(data)
@@ -15,9 +16,16 @@ var Apple = function() {
           )
  })
 	 $("#one").on("click", function(){
+   $("#zero").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#two").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#three").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#four").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#five").css('background-color', 'rgba(0, 153, 255, 0)');
 	 	$(".template").remove();
     $(".bio").text("Apple Subreddit");
 	 	$(".search").hide();
+    $("nav-but").hide();
+    $(this).css('background-color', 'rgba(0, 153, 255, 0.21)');
 	 $.getJSON(
 	 "http://www.reddit.com/r/apple/hot.json?jsonp=?",
         function foo(data)
@@ -30,11 +38,17 @@ var Apple = function() {
             }
           )
         })
- })
+     });
 	 $("#two").on("click", function(){
+    $("#zero").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#one").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#three").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#four").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#five").css('background-color', 'rgba(0, 153, 255, 0)');
 	 	 $(".template").remove();
      $(".bio").text("Google Subreddit");
 	 	 $(".search").hide();
+     $(this).css('background-color', 'rgba(0, 153, 255, 0.21)');
 	 $.getJSON(
 	 "http://www.reddit.com/r/google/hot.json?jsonp=?",
         function foo(data)
@@ -49,9 +63,15 @@ var Apple = function() {
         })
  })
 	  $("#three").on("click", function(){
+   $("#zero").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#one").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#two").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#four").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#five").css('background-color', 'rgba(0, 153, 255, 0)');
 	 	 $(".template").remove();
      $(".bio").text("Tesla Subreddit");
 	 	 $(".search").hide();
+     $(this).css('background-color', 'rgba(0, 153, 255, 0.21)');
 	 $.getJSON(
 	 "http://www.reddit.com/r/teslamotors/hot.json?jsonp=?",
         function foo(data)
@@ -66,9 +86,15 @@ var Apple = function() {
         })
  })
 	   $("#four").on("click", function(){
+    $("#zero").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#one").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#three").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#two").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#five").css('background-color', 'rgba(0, 153, 255, 0)');
 	 	 $(".template").remove();
      $(".bio").text("Facebook Subreddit");
 	 	 $(".search").hide();
+     $(this).css('background-color', 'rgba(0, 153, 255, 0.21)');
 	 $.getJSON(
 	 "http://www.reddit.com/r/facebook/hot.json?jsonp=?",
         function foo(data)
@@ -83,8 +109,14 @@ var Apple = function() {
         })
  })
 	    $("#five").on("click", function(){
+    $("#zero").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#one").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#three").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#four").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#two").css('background-color', 'rgba(0, 153, 255, 0)');
 	     var link = [];
 	     $(".template").remove();
+       $(this).css('background-color', 'rgba(0, 153, 255, 0.21)');
        $(".bio").text("");
 	     $(".feed").hide();
 	 	 $(".search").show();
@@ -116,6 +148,12 @@ var Apple = function() {
 	})
 	    console.log($(".template").length)
 	     $("#zero").on("click", function(){
+        $("two").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#one").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#three").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#four").css('background-color', 'rgba(0, 153, 255, 0)');
+    $("#five").css('background-color', 'rgba(0, 153, 255, 0)');
+        $("#zero").css('background-color', 'rgba(0, 153, 255, 0.21)')
 	     $(".search").hide();
 	 	 $(".template").remove();
 	 $.getJSON(
